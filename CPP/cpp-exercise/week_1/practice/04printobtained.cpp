de4e02ba0774afe4568b43d3ee9b6bb31a56a249
Konstantin Krasser <konstantin.krasser@student.tugraz.at>
2024-02-13 00:05:01 +0100

    initial commit

---
+++ CPP/cpp-exercise/week_1/practice/04printobtained.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // take two integers
+    int x;
+    int y;
+    cin >> x;
+    cin >> y;
+    // First calculate the sum
+    int z = x+y;
+    // Second calculate the product
+    int a = x*y;
+    // Finally print the sum of and product of both
+    cout << z << "\t" << a <<endl;
+}
